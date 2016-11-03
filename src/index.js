4d9e61cfe648ecfdedb798515f468c09192a38c9
abel <ibavca@gmail.com>
2016-11-03 16:26:44 +0500

    No error

--- src/index.js
+++ src/index.js
@@ -2,14 +2,14 @@ import React from 'react';
 import {render} from 'react-dom';
 import App from './app'
 import configureStore from './store/configureStore'
-import Provider from 'react-redux';
+import {Provider} from 'react-redux';
 
 const store = configureStore();
 
 render(
-	<Provider store={store}>
-		<App />
-	</Provider>,
-	document.getElementById('root'));
+    <Provider store={store}>
+        <App />
+    </Provider>,
+    document.getElementById('root'));
 module.hot.accept();
 
