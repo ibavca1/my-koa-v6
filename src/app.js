26a9bb728d883029a95444fcdc3f984221519c0f
abel <ibavca@gmail.com>
2016-11-03 16:11:30 +0500

    Save for home react error

--- src/app.js
+++ src/app.js
@@ -1,12 +1,21 @@
-import React, {Component}  from 'react'
+import React, {Component}  from 'react';
+import {connect} from 'react-redux';
 
 //const a = "1";
 
-export default class App extends Component {
+class App extends Component {
 	constructor(props, context){
 		super(props, context);
 	}
 	render(){
-		return <div>Hello from react.</div>
+		return <div>Hello from react from {this.props.user}.</div>
 	}
 }
+
+function mapStateToProps (state) {
+  return {
+    user: state.user
+  }
+}
+
+export default connect(mapStateToProps)(App);
--- src/index.js
+++ src/index.js
@@ -1,7 +1,15 @@
 import React from 'react';
 import {render} from 'react-dom';
 import App from './app'
+import configureStore from './store/configureStore'
+import Provider from 'react-redux';
 
-render(<App />, document.getElementById('root'));
+const store = configureStore();
+
+render(
+	<Provider store={store}>
+		<App />
+	</Provider>,
+	document.getElementById('root'));
 module.hot.accept();
 
